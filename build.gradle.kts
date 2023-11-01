75348629fdac9d54c83cb54fe108089460a5a699
Akram Louze <akramlouze2005@gmail.com>
2023-11-01 14:19:48 +0100

    Removed ConfigMe from dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,6 @@ subprojects {
     dependencies {
         implementation("com.rabbitmq:amqp-client:5.19.0")
         implementation("redis.clients:jedis:5.0.2")
-        implementation("ch.jalu.configme:1.4.1")
         implementation("com.google.code.gson:gson:2.10.1")
     }
 
